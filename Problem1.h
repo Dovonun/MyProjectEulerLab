3992e176f3c3b41352de128de7d0f147a55984f8
Severin Reifler <dovonun@gmail.com>
2019-06-09 10:01:33 +0200

    push commit nr.1 For pushing to github

---
+++ Problem1.h
@@ -0,0 +1,16 @@
+#include <vector>
+
+#ifndef LIMIT
+#define LIMIT 1000
+#endif
+
+int problem1(int value[3,5]){
+
+  vector<int> result;
+
+  for (int i = 1; i < LIMIT; i++) {
+    result.push_back(i * 3,i * 5);
+  }
+
+  return x;
+}
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "Problem1.h"
+
+int main() {
+    problem1();
+  return 0;
+}
